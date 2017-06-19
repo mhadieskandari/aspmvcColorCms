84d0e972418d750cdef6f439f307c5bad999a5de
mhadieskandari <hadi.eskandari@ymail.com>
2017-06-19 21:13:07 +0430

    after code to end of create card  . this is a good state for buy formula

---
+++ CMS_Golbarg/Areas/Admin/Models/PayPlan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CMS_Golbarg.Areas.Admin.Models
+{
+    public class PayPlan
+    {
+        public int Id { set; get; }
+
+        public string PlanName { set; get; }
+
+        public string PlanDes { set; get; }
+
+        public int Fi { set; get; }
+
+        public int Copon { set; get; }
+
+        public DateTime StartDate { set; get; }
+
+        public DateTime EndDate { set; get; }
+
+
+
+
+    }
+}
