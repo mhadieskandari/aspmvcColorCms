c384eac22678116ba3f83e92bd9df5ef3196d32e
mhadieskandari <hadi.eskandari@ymail.com>
2017-07-07 04:05:55 +0430

    after change rester and login on phonenumber base . and make login and register look better and add logo to project

--- CMS_Golbarg/Areas/Admin/Controllers/AccountController.cs
+++ CMS_Golbarg/Areas/Admin/Controllers/AccountController.cs
@@ -75,7 +75,7 @@ namespace CMS_Golbarg.Areas.Admin.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.PhoneNumber, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -151,7 +151,7 @@ namespace CMS_Golbarg.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.PhoneNumber, PhoneNumber = model.PhoneNumber };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
@@ -163,7 +163,7 @@ namespace CMS_Golbarg.Areas.Admin.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Default",new {Area="Client"});
                 }
                 AddErrors(result);
             }
@@ -202,7 +202,7 @@ namespace CMS_Golbarg.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = await UserManager.FindByNameAsync(model.Email);
+                var user = await UserManager.FindByNameAsync(model.PhoneNumber);
                 if (user == null || !(await UserManager.IsEmailConfirmedAsync(user.Id)))
                 {
                     // Don't reveal that the user does not exist or is not confirmed
@@ -248,7 +248,7 @@ namespace CMS_Golbarg.Areas.Admin.Controllers
             {
                 return View(model);
             }
-            var user = await UserManager.FindByNameAsync(model.Email);
+            var user = await UserManager.FindByNameAsync(model.PhoneNumber);
             if (user == null)
             {
                 // Don't reveal that the user does not exist
--- CMS_Golbarg/Areas/Admin/Models/AccountViewModels.cs
+++ CMS_Golbarg/Areas/Admin/Models/AccountViewModels.cs
@@ -48,10 +48,14 @@ namespace CMS_Golbarg.Areas.Admin.Models
 
     public class LoginViewModel
     {
+        //[Required]
+        //[Display(Name = "ایمیل")]
+        //[EmailAddress]
+        //public string Email { get; set; }
+
         [Required]
-        [Display(Name = "ایمیل")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "شماره موبایل")]
+        public string PhoneNumber { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -64,13 +68,17 @@ namespace CMS_Golbarg.Areas.Admin.Models
 
     public class RegisterViewModel
     {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "نام کاربری (ایمیل)")]
-        public string Email { get; set; }
+        //[Required]
+        //[EmailAddress]
+        //[Display(Name = "نام کاربری (ایمیل)")]
+        //public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "شماره موبایل")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "باید حداقل {2} کااراکتر باشد /n{0}", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "رمز عبور")]
         public string Password { get; set; }
@@ -83,13 +91,18 @@ namespace CMS_Golbarg.Areas.Admin.Models
 
     public class ResetPasswordViewModel
     {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "ایمیل")]
-        public string Email { get; set; }
+        //[Required]
+        //[EmailAddress]
+        //[Display(Name = "ایمیل")]
+        //public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+
+        [Display(Name = "شماره موبایل")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "باید حداقل {2} کااراکتر باشد /n{0}", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "رمز عبور")]
         public string Password { get; set; }
@@ -104,9 +117,14 @@ namespace CMS_Golbarg.Areas.Admin.Models
 
     public class ForgotPasswordViewModel
     {
+        //[Required]
+        //[EmailAddress]
+        //[Display(Name = "ایمیل")]
+        //public string Email { get; set; }
+
         [Required]
         [EmailAddress]
-        [Display(Name = "ایمیل")]
-        public string Email { get; set; }
+        [Display(Name = "شماره موبایل")]
+        public string PhoneNumber { get; set; }
     }
 }
